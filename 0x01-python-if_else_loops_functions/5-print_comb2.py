e49800cf1032e440676a156bf539be254d16939a
euniceabs <botweunice100@gmail.com>
2023-08-09 08:26:08 -0700

    comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num < 99:
-        print(f"{num:02}, ", end='')
+        print("{:02}, ".format(num), end='')
     else:
-        print(f"{num:02}")
+        print("{:02}".format(num))
