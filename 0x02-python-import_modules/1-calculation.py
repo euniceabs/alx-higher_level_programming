24e113308916256d7f3f2efbf73e7bbcbe8eda64
euniceabs <botweunice100@gmail.com>
2023-08-11 04:09:37 -0700

    maths calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import mul, add, sub, div
+
+    a = 10
+    b = 5
+
+    Total_mul = mul(a, b)
+    Total_add = add(a, b)
+    Total_sub = sub(a, b)
+    Total_div = div(a, b)
+
+    print("{} * {} = {}".format(a, b, Total_mul))
+    print("{} + {} = {}".format(a, b, Total_add))
+    print("{} - {} = {}".format(a, b, Total_sub))
+    print("{} / {} = {}".format(a, b, Total_div))
