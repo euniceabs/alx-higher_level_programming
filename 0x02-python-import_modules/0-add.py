9b061be77c442955a0f702a49e9707641ecbe473
euniceabs <botweunice100@gmail.com>
2023-08-11 02:54:17 -0700

    addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,10 +3,7 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    def add(a, b):
-        return a + b
-
     a = 1
     b = 2
     grand_total = add(a, b)
-    print(f'{a} + {b} = {a + b}')
+    print(f"{a} + {b} = {a + b}")
