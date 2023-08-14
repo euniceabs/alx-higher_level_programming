e720ae75030f973718469c2bb7cbf882a884ffa7
euniceabs <botweunice100@gmail.com>
2023-08-14 06:10:26 -0700

    a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,12 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+        first_element = True
+        for number in row:
+            if not first_element:
+                print(" ", end="")
+            print("{:d}".format(number), end="")
+            first_element = False
+        print()
+    if not matrix:
         print()
