d69733b93b418a6359658c0bd88b809f8c435e8b
euniceabs <botweunice100@gmail.com>
2023-08-28 08:59:43 -0700

    printing an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return False
+    return True
