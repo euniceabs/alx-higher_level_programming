e466235b1754d7f087ad4eb5f18de8fd185d56b9
euniceabs <botweunice100@gmail.com>
2023-09-12 06:32:11 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A function that has  an incompleted class
+"""
+
+
+class BaseGeometry():
+    """
+Empty class
+    """
+
+    def area(self):
+        """
+        raises an exception
+        """
+        raise Exception('area() is not implemented')
