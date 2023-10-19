4ac8c22aee22c5406e7377ebb316caab96b3312d
euniceabs <botweunice100@gmail.com>
2023-10-19 03:16:01 -0700

    sql

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- A script that displays the max temperature of each state (ordered by State name).
+-- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state;
